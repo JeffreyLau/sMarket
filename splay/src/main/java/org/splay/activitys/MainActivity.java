a152bcbb95606d33cd823a3f8e1492cb95c186b7
Jeffrey <meto.jeffrey@gmail.com>
2016-02-10 23:38:25 +0800

    renamed:    splay/src/main/java/org/splay/MainActivity.java -> splay/src/main/java/org/splay/activitys/MainActivity.java

--- splay/src/main/java/org/splay/activitys/MainActivity.java
+++ splay/src/main/java/org/splay/activitys/MainActivity.java
@@ -1,4 +1,4 @@
-package org.splay;
+package org.splay.activitys;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.splay.R;
 
 public class MainActivity extends AppCompatActivity {
 
