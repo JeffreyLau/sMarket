35329bb518190fa5b5e03b6cc76d457da13aca49
Jeffrey <meto.jeffrey@gmail.com>
2016-02-12 18:59:39 +0800

    使用Toolbar 1.隱藏ActionBar 2.AndroidMenfest中引用AppBaseTheme 3.style文件中聲明AppBaseTheme 4.佈局文件中引用Toolbar 5.Activity中使用 使用xutils註解View

--- splay/src/main/java/org/splay/activitys/MainActivity.java
+++ splay/src/main/java/org/splay/activitys/MainActivity.java
@@ -3,24 +3,26 @@ package org.splay.activitys;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import org.splay.R;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 
-public class MainActivity extends AppCompatActivity {
+@ContentView(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
 
+    @ViewInject(R.id.toolbar)
+    private Toolbar toolbar;
+    @ViewInject(R.id.fab)
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
